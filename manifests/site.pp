9c3c579c218ffd5b92dddde5aa50ea6cca8cddce
dzmitry-lab <80087668+dzmitry-lab@users.noreply.github.com>
2021-06-07 23:23:32 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -19,3 +19,25 @@ node slave1.puppet {
     replace => true,
       } 
 }
+
+node slave2.puppet {
+  Package { ensure => 'installed' }
+  $packages = [ 'httpd', 'php' ]
+  package { $packages: }
+  
+  service { 'httpd': ensure => running, enable => true, }
+  
+  file { '/root/README': ensure => absent, }
+  file { '/etc/httpd/conf.d/01-demosite-php.conf':
+    ensure => file,
+	notify  => Service['httpd'],
+    source => '/vagrant/web/apache_conf/01-demosite-php.conf',
+    replace => true,
+      } 
+  file { '/var/www/01-demosite-php': ensure => 'directory', }
+  file { '/var/www/01-demosite-php/index.php':
+    ensure => file,
+    source => '/vagrant/web/01-demosite-php/index.php',
+    replace => true,
+      }
+}
