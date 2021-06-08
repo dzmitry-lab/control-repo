c253849627c02b08e5e81f2238ecacc7476e0ca2
dzmitry-lab <80087668+dzmitry-lab@users.noreply.github.com>
2021-06-08 15:25:45 +0300

    Update mineserver.pp

--- site/profile/manifests/mineserver.pp
+++ site/profile/manifests/mineserver.pp
@@ -6,11 +6,11 @@ class profile::mineserver {
   file { '/opt/minecraft': ensure => 'directory', }
   
   include wget
-  wget::retrieve { 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar':
-    destination => '/opt/minecraft',
-    timeout     => 0,
-    verbose     => true,
-  }
+  #wget::retrieve { 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar':
+  #  destination => '/opt/minecraft',
+  #  timeout     => 0,
+  #  verbose     => true,
+  #}
   wget::retrieve { 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar':
     destination => '/home/vagrant',
     timeout     => 0,
