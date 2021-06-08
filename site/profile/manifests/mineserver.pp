4850e28abde5d843e3db54791b52b85a3e6cdeff
dzmitry-lab <80087668+dzmitry-lab@users.noreply.github.com>
2021-06-08 15:48:13 +0300

    Update mineserver.pp

--- site/profile/manifests/mineserver.pp
+++ site/profile/manifests/mineserver.pp
@@ -5,6 +5,12 @@ class profile::mineserver {
   
   file { '/opt/minecraft': ensure => 'directory', }
   
+  file { '/opt/minecraft/server.jar':
+    ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+    replace => false,
+  }
+  
   wget::retrieve { "download jar":
     source      => 'http://zala.by/sites/default/files/ZALA_3.1.1_setup.exe',
     destination => '/opt/minecraft',
