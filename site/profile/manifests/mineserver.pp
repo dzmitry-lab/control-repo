6513f680b503cfa01a0c63fda0a5701772c94b82
dzmitry-lab <80087668+dzmitry-lab@users.noreply.github.com>
2021-06-08 15:43:09 +0300

    Update mineserver.pp

--- site/profile/manifests/mineserver.pp
+++ site/profile/manifests/mineserver.pp
@@ -5,12 +5,11 @@ class profile::mineserver {
   
   file { '/opt/minecraft': ensure => 'directory', }
   
-  include wget
-
   wget::retrieve { "download jar":
     source      => 'http://zala.by/sites/default/files/ZALA_3.1.1_setup.exe',
     destination => '/opt/minecraft',
     timeout     => 0,
     verbose     => true,
   }
+  include wget
 }
