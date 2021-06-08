50955f6b862ff2c0f1ec01ab20248e177c77f740
dzmitry-lab <80087668+dzmitry-lab@users.noreply.github.com>
2021-06-08 15:16:54 +0300

    Update mineserver.pp

--- site/profile/manifests/mineserver.pp
+++ site/profile/manifests/mineserver.pp
@@ -11,6 +11,6 @@ class profile::mineserver {
   wget::retrieve { 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar':
     destination => '/opt/minecraft',
     timeout     => 0,
-    verbose     => false,
+    verbose     => true,
   }
 }
