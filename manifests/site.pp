09b640396e56b88adbf2f3cc57c68002fb928045
dzmitry-lab <80087668+dzmitry-lab@users.noreply.github.com>
2021-06-10 22:23:45 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -22,13 +22,15 @@ node slave1.puppet {
   file { '/etc/httpd/conf.d/01-demosite-static.conf':
     notify  => Service['httpd'],
     ensure => file,
-    source => '/vagrant/web/apache_conf/01-demosite-static.conf',
+    source => 'puppet:///modules/dev_conf/01-demosite-static.conf',
+#    source => '/vagrant/web/apache_conf/01-demosite-static.conf',
     replace => true,
       } 
   file { '/var/www/01-demosite-static': ensure => 'directory', }
   file { '/var/www/01-demosite-static/index.html':
     ensure => file,
-    source => '/vagrant/web/01-demosite-static/index.html',
+    source => 'puppet:///modules/dev_web/index.html',
+ #   source => '/vagrant/web/01-demosite-static/index.html',
     replace => true,
       } 
 }
@@ -44,13 +46,15 @@ node slave2.puppet {
   file { '/etc/httpd/conf.d/01-demosite-php.conf':
     ensure => file,
 	notify  => Service['httpd'],
-    source => '/vagrant/web/apache_conf/01-demosite-php.conf',
+    source => 'puppet:///modules/dev_conf/01-demosite-php.conf',
+#    source => '/vagrant/web/apache_conf/01-demosite-php.conf',
     replace => true,
       } 
   file { '/var/www/01-demosite-php': ensure => 'directory', }
   file { '/var/www/01-demosite-php/index.php':
     ensure => file,
-    source => '/vagrant/web/01-demosite-php/index.php',
+    source => 'puppet:///modules/dev_web/index.php',
+#    source => '/vagrant/web/01-demosite-php/index.php',
     replace => true,
       }
 }
